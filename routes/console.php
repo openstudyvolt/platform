b57a6e75d5e964ca514d4d30920f412539dde81b
Hicham SADDEK <36214361+hicham-saddek@users.noreply.github.com>
2025-06-27 19:36:41 +0100

    Add spatie medialibrary and S3 support

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('s3:list', function () {
+    $disk = Storage::disk('s3');
+
+    foreach ($disk->files('') as $file) {
+        $this->line($file);
+    }
+})->purpose('List files in the configured S3 bucket');
